fc1f26a3ca6facd19ad7ee31014088be8bb0ae9c
Micaela Pedemonti <micaelapedemonti@Micaelas-MacBook-Pro.local>
2022-12-28 23:50:22 -0300

    agregando paginas

--- src/components/Peliculas/Peliculas.js
+++ src/components/Peliculas/Peliculas.js
@@ -1,24 +1,29 @@
 import SearchIcon from '@mui/icons-material/Search';
+import logo from "../../logo-netflix.png"
+import './Peliculas.css';
 
 const Peliculas = () =>{
     return (
         <div className="header">
-         <nav>
-            <ul>
-                <li><a href="">Popular </a></li>
-                <li><a href="">Documentales</a></li>
-                <li><a href="">Kids</a></li>
-                <li><a href="">Drama</a></li>
-                <li><a href="">Comedia</a></li>
+            <div className='menu_header'>
+          <div className='logo'>
+             <img src={logo} width="100" height="30" className='logo_img' alt="logo" />
+          </div>
+         <nav className='menu_container'>
+            <ul className='menu_navegacion'>
+                <li><a href="">Series </a></li>
+                <li><a href="">Peliculas</a></li>
+                <li><a href="">Novedades populares</a></li>
             </ul>
-         </nav>
-         <form>
+            <form>
             <div className="search_btn">
-            <SearchIcon>
-                <input type="text" placeholder="Ingrese una pelicula" className="input_search"> </input>
-            </SearchIcon>
+            <input type="text" placeholder="Ingrese una pelicula" className="input_search" />
+            <SearchIcon className='search_icon' fontSize='large'/>
+          
             </div>
-         </form>
+            </form>
+         </nav>
+         </div>
         </div>
     )
 }
